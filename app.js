28d4266c883114c35c20551a160f3d465f67af21
Gourab Chatterjee <gourabchatterjee623@gmail.com>
2023-02-21 10:41:46 +0530

    update106

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cron = require('node-cron');
 const url = 'https://www.careerswave.in/ei-samay-epaper-pdf/';
 console.log('starting');
 // Schedule the ask to run every day at 7 am
-cron.schedule('12 10 * * *', () => {
+cron.schedule('50 10 * * *', () => {
   https
     .get(url, (res) => {
       let data = '';
